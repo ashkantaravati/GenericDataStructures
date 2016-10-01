4b7c34709cf8f014bce3df931ea9bd27814e7fcc
Ashkan Taravati <ashkan_taravati@hotmail.com>
2016-10-02 00:16:14 +0330

    First Commit
    Implements a Simple Generic Stack

---
+++ GenericDataStructures/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CACTB.GenericDataStructures;
+
+namespace GenericDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfStack;
+            Console.WriteLine("Please Enter size of Stack");
+           sizeOfStack=int.Parse( Console.ReadLine());
+            var stk = new Stack<string>(sizeOfStack);
+            string words = Console.ReadLine();
+            var x = words.Split(' ');
+            foreach (var str in x)
+            {
+                stk.Push(str);
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(stk.Pop());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GenericDataStructures/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CACTB.GenericDataStructures
+{
+    public class Stack<T>
+    {
+        int Index;
+        T[] Container;
+        int Size;
+        public Stack(int size)
+        {
+            Size = size;
+            Container = new T[size];
+            Index = -1;
+        }
+       public T Pop()
+        {
+            if (Index == -1)
+                throw new Exception("Stack is Empty!");
+            else return Container[Index--];
+        }
+        public void Push(T element)
+        {
+            if (Index == Size)
+                throw new Exception("Stack is Full!");
+            else
+            {
+                Index++;
+                Container[Index] = element;
+            }
+        }
+    }
+   
+}
